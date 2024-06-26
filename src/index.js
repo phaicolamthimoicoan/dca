d6cf24a6e0135aeffd069fc1be5ca65c189d198a
Citnut <chinhchot000@gmail.com>
2024-06-26 15:13:44 +0700

    client method: use (onLogin function)

--- src/index.js
+++ src/index.js
@@ -20,7 +20,10 @@ export default function DCAlogin(config) {
 
   client.once(Events.Error, console.error);
   client.once(Events.Warn, console.warn);
-  client.once(Events.ClientReady, () => console.log(" \x1b[32m[Citnut]\x1b[0m DCA is ready!"))
+  client.once(Events.ClientReady, () => { 
+    console.log(" \x1b[32m[Citnut]\x1b[0m DCA is ready!");
+    client.load()
+  })
 
   return client
 }
--- src/models/client.js
+++ src/models/client.js
@@ -4,17 +4,50 @@ import { Client as DefaultClient, Events } from "discord.js";
 import DefaultClientOptions from "../default/clientOptions.js";
 
 export default class Client extends DefaultClient {
+  /**
+   * 
+   * @param {typeof DefaultClientOptions} options 
+   */
   constructor(options = {}) {
     super(Object.assign(DefaultClientOptions, options));
+    /**
+     * @type {Map<string, execute(import("discord.js").Interaction) =>void>}
+     */
     this.commands = new Map()
   };
-
+  /**
+   * 
+   * @param {(data: import("discord.js").Message)=>void} listener 
+   * @returns {this}
+   */
   listenMsg(listener) {
     return this.on(Events.MessageCreate, listener)
   };
-
+  /**
+   * 
+   * @param {(data: import("discord.js").Interaction)=>void} listener 
+   * @returns {this}
+   */
   listenCmd(listener) {
     return this.on(Events.InteractionCreate, listener)
   };
+  /**
+   * @type {Array<(that: this)=>void>}
+   */
+  #onLoad = [];
+  /**
+   * 
+   * @param {(that: this)=>void} func 
+   */
+  use(func) {
+    this.#onLoad.push(func)
+  };
+  load() {
+    for (const once of this.#onLoad) {
+      try {
+        once(this)
+      } catch { console.error }
+    }
+  }
 }
 
